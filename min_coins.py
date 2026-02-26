dd07a7a6caf11a26916e38e8a525e95d6495a874
Kai Sherman <63990664+Kaishermy@users.noreply.github.com>
2026-02-26 08:47:00 -0600

    Refactor find_min_coins function and restore tests

--- min_coins.py
+++ min_coins.py
@@ -1,19 +1,3 @@
-# def find_min_coins(denominations, target, subset):
-#     denominations.sort(reverse=True)
-#
-#     if target == 0:
-#         return True
-#
-#     for coin in denominations:
-#         if coin <= target:
-#             subset.append(coin)
-#             result = find_min_coins(denominations, target - coin, subset)
-#             if result:
-#                 return subset
-#             subset.pop()
-#
-#     return []
-
 def find_min_coins(denominations, target, curr_subset):
     if target == 0:
         return curr_subset
@@ -31,3 +15,4 @@ def find_min_coins(denominations, target, curr_subset):
 print(find_min_coins([1, 5, 2, 8, 10], 20, []))  # [10, 10]
 print(find_min_coins([1, 5, 2, 8, 10], 16, []))  # [8, 8]
 print(find_min_coins([1, 5, 2, 8, 10], 17, []))  # [10, 5, 2]
+
